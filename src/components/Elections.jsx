120ccfe024401db23ce108a9e88eff1e6240b517
yusuf <kingshakzy@gmail.com>
2023-08-14 20:16:47 +0100

    Great modals erect

--- src/components/Elections.jsx
+++ src/components/Elections.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+import { useState} from 'react'
 import { Text, Box, Flex, Spacer,
     Button,
     Modal,
@@ -6,16 +8,51 @@ import { Text, Box, Flex, Spacer,
     ModalHeader,
     ModalFooter,
     ModalBody,
-    ModalCloseButton,
     useDisclosure
  } from "@chakra-ui/react";
- import { FormHelperText } from '@chakra-ui/react'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input
+  } from '@chakra-ui/react';
+  import { Field, Form, Formik } from "formik";
 import { Colors } from "../assets/constants/colors";
 import OLiner from "../assets/images/oldLineer.png";
-import Liner from "../assets/images/newLiner.png"
+import { useAsyncValue } from 'react-router-dom';
+// import Liner from "../assets/images/newLiner.png"m1i9zzzzz
 export const Elections = (props) =>{
     const {isOpen, onOpen, onClose } = useDisclosure();
-    return(
+    const [regState, setRegState] = useState(false);
+    const userNameValidate = (value) =>{
+        let error;
+        value = value.trim();
+        if(value === ""){
+            error = "Username is required"
+        }else if(value.length <= 2){
+            error = "Username should be more than 2 characters long"
+        }
+        else{
+            error = "";
+        }
+        return error
+    }
+    const emailValidate = (value) =>{
+        // let error;
+        let myRegex = /[a-z+|0-9+]@[a-z]+.com/ig
+        let error = myRegex.test(value);
+        value = value.trim();
+        if(value === "" || value.length <=2){
+            return "Email is required"
+        }
+        else if(error){
+            return ""
+        }else{
+            return "Email is invalid"
+        }
+    }
+     return(
         <>
          <Modal isOpen={isOpen} size={'4xl'}  onClose={onClose}>
         <ModalOverlay />
@@ -32,7 +69,104 @@ export const Elections = (props) =>{
           {/* <ModalCloseButton /> */}
           <ModalBody>
            <Flex flexDir={"column"} alignItems={'center'} >
-                <Button width={'15rem'} color={'white'} bgColor={Colors.primary}>
+                { regState &&
+                <Formik
+                initialValues={{
+                    email: "",
+                    username: '',
+                    password: ""
+                }}
+                validateOnChange={true}
+                >
+                    {(props) => (
+                        <Form
+                        style={{width: '50%', marginBottom: "1rem"}}
+                        >
+                            <Field name="email" validate={emailValidate}>
+                                {(props) =>(
+                                    <FormControl isInvalid={
+                                        props.form.errors.email && props.form.touched.email
+                                    }>
+                                        <FormLabel
+                                            ml={"0.3rem"}
+                                            fontWeight={"semibold"}
+                                            fontSize={"16px"}
+                                         >
+                                            Email
+                                        </FormLabel>
+                                        <Input
+                                        {...props.field}
+                                        autoComplete={"true"}
+                                        type="email"
+                                        placeholder="gethan@gmail.com"
+                                        >
+                                        </Input>
+                                        <FormErrorMessage w={"full"} textColor={"red"}>
+                                            {props.form.errors.email}
+                                        </FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+                            <Field 
+                            name="username"
+                            validate={userNameValidate}
+                            >
+                                {(props) =>(
+                                    <FormControl 
+                                    isInvalid = {props.form.errors.username && props.form.touched.username}
+                                    >
+                                        <FormLabel
+                                        ml={'0.3rem'}
+                                        fontWeight={'semibold'}
+                                        fontSize={'16px'}
+                                        >
+                                            Username
+                                        </FormLabel>
+                                        <Input
+                                        {...props.field}
+                                        type="text"
+                                        placeholder="Gethan"
+                                        autoComplete={"false"}
+                                        >
+                                        </Input>
+                                        <FormErrorMessage w="full" textColor="red">
+                                            {props.form.errors.username}
+                                        </FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+                            <Field name="password">
+                                {(props)=>(
+                                    <FormControl
+                                    isInvalid={props.form.errors.password && props.form.touched.password}
+                                    >
+                                        <FormLabel
+                                        ml={'0.3rem'}
+                                        fontWeight={'semibold'}
+                                        fontSize={'16px'}
+                                        >
+                                            Password
+                                        </FormLabel>
+                                        <Input
+                                        {...props.field}
+                                        type="password"
+                                        placeholder="--------"
+                                        autoComplete={"false"}
+                                        >
+                                        </Input>
+                                    </FormControl>
+                                )}
+                            </Field>
+                        </Form>
+                    )}
+                </Formik>
+                }
+                <Button onClick={ regState ? () =>{
+                        return console.log('aarg')
+                        }
+                    :
+                () => setRegState(!regState)
+                } width={'15rem'} color={'white'} bgColor={Colors.primary}>
                     Register for this election
                 </Button>
                 <Flex flexDir={'row'} alignItems={'center'} justifyContent={'center'}>
@@ -45,6 +179,7 @@ export const Elections = (props) =>{
                 </Flex>
                
             </Flex>
+            
           </ModalBody>
 
           <ModalFooter>
